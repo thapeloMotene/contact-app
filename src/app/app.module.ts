b95170517ee8fff5068022a521616ca03538da9d
Thapelo Motene <thapelo.motene@gmail.com>
2020-09-20 21:25:13 +0200

    completed: list item container

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,20 +6,25 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
 
+import {MatListModule} from '@angular/material/list';
+
 import { InMemoryWebApiModule } from 'angular-in-memory-web-api'; 
 
 import {DataStoreService} from './data-store.service';
 import {ApiHttpServiceService} from './api-http-service.service';
+import { ContactListComponent } from './components/contact-list/contact-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContactListComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     HttpClientModule,
+    MatListModule,
     InMemoryWebApiModule.forRoot(DataStoreService)
   ],
   providers: [ApiHttpServiceService],
