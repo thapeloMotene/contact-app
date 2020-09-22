40967cc57832defad5539b95825569b3e97dc90b
Thapelo Motene <thapelo.motene@gmail.com>
2020-09-22 23:24:56 +0200

    completed:search functionality

--- src/app/screens/home/home.component.ts
+++ src/app/screens/home/home.component.ts
@@ -9,11 +9,23 @@ import {ApiHttpService} from '../../api-http-service.service';
 export class HomeComponent implements OnInit {
 
 contacts: any;
+filteredContacts:any;
+searchKey:string;
 
   constructor(private _api: ApiHttpService){
     this.contacts = this._api.getContacts().subscribe(result =>{
         this.contacts = result;
+        this.filteredContacts= result;
     });
+
+  }
+
+
+  onKeyUp(e){
+    console.log("[keyup]:",e.target.value);
+    this.searchKey = e.target.value;
+
+    this.filteredContacts = this.contacts.filter(contact => {return contact['name'].includes(this.searchKey)  || contact['surname'].includes(this.searchKey)})
 
   }
 
